4f34dfe868f83a4bb899802b33d4470ea4926b8c
Bikram Ghuku <bikramghuku05@gmail.com>
2024-03-07 20:21:31 +0530

    chore: added userRouter and bodyParser JSON to app

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import router from "./routes/User";
+import bodyParser from "body-parser"
 
 dotenv.config()
 
 const app : Express = express()
 
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).send("Hello there")
-})
+app.use(bodyParser.json())
+app.use("/", router)
 
 app.listen(PORT, () => {
     console.log("Server listening on: ", PORT)
